7421fb5820394afc6cbdf97ee7ee169ff2154012
Marcus Blagdon <marcus.blagdon64@ed.cna.nl.ca>
2024-01-11 13:29:39 -0500

    Barebones for the website, getting things situated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import EquipmentList from './pages/EquipmentList';
+import MaintenanceSchedule from './pages/MaintenanceSchedule';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <h1>Drone Maintenance</h1>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/equipment" element={<EquipmentList />} />
+                    <Route path="/maintenance" element={<MaintenanceSchedule />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
+
---
+++ frontend/src/pages/EquipmentList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EquipmentList() {
+    return (
+        <div>
+            <h2>Equipment List</h2>
+            <p>List of all drone equipment will be displayed here.</p>
+            {/* Code to display equipment list goes here */}
+        </div>
+    );
+}
+
+export default EquipmentList;
+
