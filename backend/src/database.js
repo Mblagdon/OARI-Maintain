3b52f9ba9ac521ad05a28f044c9d05d00dcdce4e
Marcus Blagdon <marcus.blagdon64@ed.cna.nl.ca>
2024-01-15 07:40:49 -0500

    fixed issue with db not connecting

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+console.log(process.env.DB_NAME);
 const mysql = require('mysql2');
 
 const dbConnection = mysql.createConnection({
-    host: '',
-    user: '',
-    password: '',
-    database: '',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 // Connect to the database
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', equipmentRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
