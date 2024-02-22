b96115cafa78213992aec5539163702c79c37029
Marcus Blagdon <marcus.blagdon64@ed.cna.nl.ca>
2024-02-22 07:52:07 -0500

    updated index.js to run on older browsers if necessary

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,6 +6,18 @@ import App from './App';
 import { MsalProvider } from '@azure/msal-react';
 import { msalInstance } from './components/oauth/msalService';
 import { UserProfileProvider } from './pages/UserProfileContext';
+
+// Polyfill for window.crypto.randomUUID
+if (!window.crypto.randomUUID) {
+    window.crypto.randomUUID = function() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (Math.random() * 16) | 0,
+                v = c === 'x' ? r : (r & 0x3) | 0x8;
+            return v.toString(16);
+        });
+    };
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
