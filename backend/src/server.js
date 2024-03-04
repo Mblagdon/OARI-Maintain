eea81694bf178bd04f723639b653bbc1fbcaef0e
Marcus Blagdon <marcus.blagdon64@ed.cna.nl.ca>
2024-03-04 07:14:54 -0500

    working on login

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,13 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.enable('trust proxy');
 
-// Redirect HTTP to HTTPS
-app.use((req, res, next) => {
-    if (req.protocol === 'http') {
-        return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-    next();
-});
+
 
 // Routes
 app.use('/api', equipmentRoutes);
